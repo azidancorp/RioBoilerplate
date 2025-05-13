38d39510472dc1ff091ed4097dad91613ff9323c
azidancorp <azidancorp@gmail.com>
2025-05-13 01:51:27 +0100

    move user db to data folder

--- app/app/persistence.py
+++ app/app/persistence.py
@@ -24,7 +24,7 @@ class Persistence:
     `db_path`: Path to the SQLite database file
     """
 
-    def __init__(self, db_path: Path = Path("user.db")) -> None:
+    def __init__(self, db_path: Path = Path("app", "data", "users.db")) -> None:
         """
         Initialize the Persistence instance and ensure necessary tables exist.
         """
